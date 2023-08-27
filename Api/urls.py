20ca323b7967f756104823e4d22a2f717912043c
kushagra sharma <sparrow.kushagra@gmail.com>
2023-08-28 00:39:53 +0530

    [ + ]  API Supportd Added

--- Api/urls.py
+++ Api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -7,13 +10,18 @@ from drf_spectacular.views import (
 
 from . import views
 
+router = SimpleRouter()
+router.register('v1/users', views.UsersViewSet, basename='users')
+router.register('v1/blogs', views.BlogViewSet, basename='blogs')
+
+
 urlpatterns = [
     path('', views.APIHome.as_view(), name='apihome'),
-    path('v1/', views.BlogViewSet.as_view({'get':'list'}),name='apibloglist' ),
-    path('v1/users/', views.UsersViewSet.as_view({'get': 'list'}), name='apiusers'),
+    path('v1/', SpectacularSwaggerView.as_view(url_name='schema'), name='apibloglist' ),
     path('v1/auth/', include('dj_rest_auth.urls')),
     path('v1/auth/signup/', include('dj_rest_auth.registration.urls')),
+    path('auth/', include('rest_framework.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='schema_redoc'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='schema_swagger'),
-]
+] + router.urls
