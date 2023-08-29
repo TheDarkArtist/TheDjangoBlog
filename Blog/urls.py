a3453424a732bb6736b65664011df4de84f7a75a
Kushagra Sharma <sparrow.kushagra@gmail.com>
2023-08-29 08:58:33 +0530

    [ + ]  Blog CRUD, navbar for useful blog releted links" "

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,8 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('blog/', views.BlogListView.as_view(), name='blog'),
-    path('blog/<pk>', views.BlogDetailView.as_view(), name='blog_detail'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('blog/', views.BlogListView.as_view(), name='blog'),
+    path('blog/create/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('blog/update/<pk>/', views.BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/delete/<pk>', views.BlogDeleteView.as_view(), name='blog_delete'),
+    path('blog/<pk>', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/search/', views.BlogSearchView.as_view(), name='blog_search'),
     
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,17 +1,55 @@
-from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic import TemplateView, DetailView, ListView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import BlogPost
 
 class HomeView(ListView):
     model = BlogPost
+    fields = '__all__'
     context_object_name = 'blogs'
     template_name = "home.html"
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    fields = ('title', 'description')
+    success_url = reverse_lazy('blog')
+    template_name = "blog_create.html"
+    def form_valid(self, form):
+        form.instance.author= self.request.user
+        return super().form_valid(form)
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    success_url = reverse_lazy('blog')
+    template_name = "blog_delete.html"
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    fields = ('title', 'description')
+    success_url = reverse_lazy('blog')
+    template_name = "blog_update.html"
+
+
 
 class BlogListView(ListView):
     model = BlogPost
     context_object_name = 'blogs'
     template_name = "blog.html"
 
+
+class BlogSearchView(ListView):
+    model = BlogPost
+    context_object_name = 'blogs'
+    template_name = "blog_search.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get('title')
+        if query:
+            object_list = self.model.objects.filter(title__icontains=query)
+        else:
+            object_list = self.model.objects.none()
+        return object_list
+    
 
 class BlogDetailView(DetailView):
     model = BlogPost
